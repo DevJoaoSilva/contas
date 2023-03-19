e69f1e0a1771af2082f656500908325b57fa1c0f
DevJoaoSilva <joaosilvadev.93@gmail.com>
2023-03-19 22:10:17 +0000

    upd

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
-
 function myFunction(mediachange) { // Media change - Menu script
     if (mediachange.matches) {
         document.querySelector('#menu-box').style.display = 'none'
         window.addEventListener('click', (event) => {
             var cle = event.target['id'] 
-            if(cle!='menu-box' && cle!='menu-logo' && mediachange.matches){
+            if(cle!='menu-box' && cle!='menu-actv' && mediachange.matches){
                 document.querySelector('#menu-box').style.display = 'none'
                 document.querySelector('#content-section').style.filter = 'none'
             }
-            else if(cle=='menu-logo'){ 
+            else if(cle=='menu-actv'){ 
                 document.querySelector('#menu-box').style.display = 'block'
                 document.querySelector('#content-section').style.filter = 'brightness(40%)'
             }
@@ -18,8 +17,40 @@ function myFunction(mediachange) { // Media change - Menu script
         })
    }else{ 
     document.querySelector('#menu-box').style.display = 'block'
+    document.querySelector('#content-section').style.filter = 'none'
+ }
+}
+var mediachange = window.matchMedia("(max-width: 992px)")
+myFunction(mediachange) // Call listener function at run time
+mediachange.addEventListener("change", myFunction) // Attach listener function on state change
+
+
+
+
+
+/*function myFunction(mediachange) { // Media change - Menu script
+    if (mediachange.matches) {
+        document.querySelector('#menu-box').style.display = 'none'
+        window.addEventListener('click', (event) => {
+            var cle = event.target['id'] 
+            if(cle!='menu-box' && cle!='menu-actv' && mediachange.matches){
+                document.querySelector('#menu-box').style.display = 'none'
+                document.querySelector('#content-section').style.filter = 'none'
+            }
+            else if(cle=='menu-actv'){ 
+                document.querySelector('#menu-box').style.display = 'block'
+                document.querySelector('#content-section').style.filter = 'brightness(40%)'
+            }
+            document.querySelector('#menu-box').addEventListener('click', function(event) {
+            event.stopPropagation();
+            });
+        })
+   }else{ 
+    document.querySelector('#menu-box').style.display = 'block'
+    document.querySelector('#content-section').style.filter = 'none'
  }
 }
 var mediachange = window.matchMedia("(max-width: 992px)")
 myFunction(mediachange) // Call listener function at run time
 mediachange.addListener(myFunction) // Attach listener function on state change
+*/
